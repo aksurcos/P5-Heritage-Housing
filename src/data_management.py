2259eaf77306875cb7c301e5c346e3c8cf989bf2
Aysenur Coskuner <aysenurkubrasurucu@gmail.com>
2024-12-14 20:54:08 +0000

    deprecated @st.cache_resource error solved, new version added

--- src/data_management.py
+++ src/data_management.py
@@ -4,7 +4,7 @@ import numpy as np
 import joblib
 
 
-@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+@st.cache_resource
 def housing_data():
     df = pd.read_csv("/workspace/P5-Heritage-Housing/outputs/datasets/collection/cleaned/CleanedHousePrices.csv")
     return df
@@ -12,7 +12,6 @@ def housing_data():
 def inherited_house_data():
     in_df = pd.read_csv("/workspace/P5-Heritage-Housing/outputs/datasets/collection/inherited_houses.csv")
     return in_df
-
 
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
