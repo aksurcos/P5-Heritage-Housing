4ba7cff4fe7b8b0a9b3c4fee66ac11f880a2fcb5
Aysenur Coskuner <aysenurkubrasurucu@gmail.com>
2024-12-14 21:08:40 +0000

    correlation study page picking info process

--- app_pages/correlation_page.py
+++ app_pages/correlation_page.py
@@ -7,4 +7,11 @@ def correlation_page_body():
     df = housing_data()
     st.write("## Correlation Analysis :bar_chart:")
     st.write(df.head(10))
+    st.write("### Pearson & Spearman Correlation Plots")
+    st.write("#### Pearson")
+    pearson = plt.imread('/workspace/P5-Heritage-Housing/outputs/study/pearson.png')
+    st.image(pearson)
+    st.write("#### Spearman")
+    spearman = plt.imread('/workspace/P5-Heritage-Housing/outputs/study/spearman.png')
+    st.image(pearson)
     
